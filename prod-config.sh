2b0ea8c5333f316bc709b3e3b43c9bf202534622
Géraldine de Vanssay <geraldine.devanssay@epitech.eu>
2018-12-02 19:15:30 +0100

    docker-compose + config

---
+++ prod-config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cat -e src/intra_gerant/src/utils/Paths.js | grep HOST
+sed -i '2s/HOST:.*/HOST:"\/\/api.centrale-fitness.fr.nf",/' src/intra_gerant/src/utils/Paths.js
+cat -e src/intra_gerant/src/utils/Paths.js | grep HOST
+echo "\n"
+cat -e src/intra_admin/src/utils/Paths.js | grep HOST
+sed -i '5s/HOST:.*/HOST:"\/\/api.centrale-fitness.fr.nf",/' src/intra_admin/src/utils/Paths.js
+cat -e src/intra_admin/src/utils/Paths.js | grep HOST
--- recup-repo.sh
+++ recup-repo.sh
@@ -6,7 +6,7 @@ mkdir src
 
 cd src
 
-for repo in $(cat "../repo_name.txt"); do
+for repo in $(cat "../repo-name.txt"); do
 	echo ${repo}
 	git clone "https://github.com/CentraleFitness/$repo"
 done
