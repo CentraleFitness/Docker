a6c2afef2d4def656c9554256d90e9c785f33ff9
Géraldine de Vanssay <geraldine.devanssay@epitech.eu>
2018-12-02 19:54:09 +0100

    fix docker-compose

--- prod-config.sh
+++ prod-config.sh
@@ -3,7 +3,7 @@
 cat -e src/intra_gerant/src/utils/Paths.js | grep HOST
 sed -i '2s/HOST:.*/HOST:"\/\/api.centrale-fitness.fr.nf",/' src/intra_gerant/src/utils/Paths.js
 cat -e src/intra_gerant/src/utils/Paths.js | grep HOST
-echo "\n"
+echo;
 cat -e src/intra_admin/src/utils/Paths.js | grep HOST
 sed -i '5s/HOST:.*/HOST:"\/\/api.centrale-fitness.fr.nf",/' src/intra_admin/src/utils/Paths.js
 cat -e src/intra_admin/src/utils/Paths.js | grep HOST
