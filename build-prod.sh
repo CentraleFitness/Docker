9d089fc517cdc4e651d95effcb0d528c4da53172
Géraldine de Vanssay <geraldine.devanssay@epitech.eu>
2018-12-05 00:49:24 +0100

    add java

--- build-prod.sh
+++ build-prod.sh
@@ -2,42 +2,42 @@
 
 set -e
 
-docker build --build-arg CURRENT_APP=site_vitrine -t "nginx-prod-site-vitrine" -f nginx/prod/Dockerfile .
-docker build --build-arg CURRENT_APP=intra_gerant -t "nginx-prod-intra-gerant" -f nginx/prod/Dockerfile .
-docker build --build-arg CURRENT_APP=intra_admin -t "nginx-prod-intra-admin" -f nginx/prod/Dockerfile .
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker build -t "java-prod-server" -f java/prod/Dockerfile .
+docker build --build-arg CURRENT_APP=site_vitrine -t "nginx-prod-site-vitrine" -f $DIR/nginx/prod/Dockerfile .
+docker build --build-arg CURRENT_APP=intra_gerant -t "nginx-prod-intra-gerant" -f $DIR/nginx/prod/Dockerfile .
+docker build -t server-java -f $DIR/java/prod/Dockerfile .
 
 #build de relay-services Dockerfile, changement en perspective pour ajouter les autres projets python
-docker build -t "python-prod-relay-service" \
-        --build-arg APP=relay-services \
-        --build-arg REQUIREMENT_FOLDER=client \
-        --build-arg ENTRYPOINT_APP=api_main.py \
-        -f python/Dockerfile .
+#docker build -t "python-prod-relay-service" \
+#        --build-arg APP=relay-services \
+#        --build-arg REQUIREMENT_FOLDER=client \
+#        --build-arg ENTRYPOINT_APP=api_main.py \
+#        -f python/Dockerfile .
 
-docker build -t "python-prod-api-manager-email" \
-        --build-arg APP=api-manager \
-        --build-arg REQUIREMENT_FOLDER=email \
-        --build-arg ENTRYPOINT_APP=runserver.py \
-        -f python/Dockerfile .
+#docker build -t "python-prod-api-manager-email" \
+#        --build-arg APP=api-manager \
+#        --build-arg REQUIREMENT_FOLDER=email \
+#        --build-arg ENTRYPOINT_APP=runserver.py \
+#        -f python/Dockerfile .
 
-docker build -t "python-prod-api-manager-showcase" \
-        --build-arg APP=api-manager \
-        --build-arg REQUIREMENT_FOLDER=showcase \
-        --build-arg ENTRYPOINT_APP=runserver.py \
-        -f python/Dockerfile .
+#docker build -t "python-prod-api-manager-showcase" \
+#        --build-arg APP=api-manager \
+#        --build-arg REQUIREMENT_FOLDER=showcase \
+#        --build-arg ENTRYPOINT_APP=runserver.py \
+#        -f python/Dockerfile .
 
-docker build -t "python-prod-backoffice-relais" \
-        --build-arg APP=backoffice-relais \
-        --build-arg REQUIREMENT_FOLDER=backoffice/backoffice \
-        --build-arg ENTRYPOINT_APP=manage.py \
-        -f python/Dockerfile .
+#docker build -t "python-prod-backoffice-relais" \
+#        --build-arg APP=backoffice-relais \
+#        --build-arg REQUIREMENT_FOLDER=backoffice/backoffice \
+#        --build-arg ENTRYPOINT_APP=manage.py \
+#        -f python/Dockerfile .
 
-docker build -t "python-prod-backoffice-server" \
-        --build-arg APP=backoffice-server \
-        --build-arg REQUIREMENT_FOLDER=. \
-        --build-arg ENTRYPOINT_APP=manage.py \
-        -f python/Dockerfile .
+#docker build -t "python-prod-backoffice-server" \
+ #       --build-arg APP=backoffice-server \
+  #      --build-arg REQUIREMENT_FOLDER=. \
+   #     --build-arg ENTRYPOINT_APP=manage.py \
+    #    -f python/Dockerfile .
 
 #docker build -t "python-prod-email-api" \
  #       --build-arg APP=email_api \
@@ -45,14 +45,14 @@ docker build -t "python-prod-backoffice-server" \
    #     --build-arg ENTRYPOINT_APP=manage.py \
     #    -f python/Dockerfile .
 
-docker build -t "python-prod-geocoding-gps-api" \
-        --build-arg APP=geocoding \
-        --build-arg REQUIREMENT_FOLDER=gps_api \
-        --build-arg ENTRYPOINT_APP=gps_api.py \
-        -f python/Dockerfile .
+#docker build -t "python-prod-geocoding-gps-api" \
+ #       --build-arg APP=geocoding \
+  #      --build-arg REQUIREMENT_FOLDER=gps_api \
+   #     --build-arg ENTRYPOINT_APP=gps_api.py \
+    #    -f python/Dockerfile .
 
-docker build -t "python-prod-geocoding-gym-referencer" \
-        --build-arg APP=geocoding \
-        --build-arg REQUIREMENT_FOLDER=gym_referencer \
-        --build-arg ENTRYPOINT_APP=gym_referencer.py \
-        -f python/Dockerfile .
+#docker build -t "python-prod-geocoding-gym-referencer" \
+ #       --build-arg APP=geocoding \
+  #      --build-arg REQUIREMENT_FOLDER=gym_referencer \
+   #     --build-arg ENTRYPOINT_APP=gym_referencer.py \
+    #    -f python/Dockerfile .
--- build-src.sh
+++ build-src.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 mkdir -p dist
 mkdir -p src
 
@@ -9,26 +11,18 @@ mkdir -p src
 
 #docker rm $(docker ps -aq -f name=nginx-build)
 
-#docker build -t "fit/java-build" -f $DIR/java/build/Dockerfile .
-#docker run "fit/java-build" -n "fit/java-build/run"
+docker build -t "fit/java-build" -f $DIR/java/build/Dockerfile .
+docker run --name "java-build" "fit/java-build"
+docker cp "java-build":/opt/app/build/libs/ dist/server
+docker cp "java-build":/opt/app/ServerSettings.ini dist/server
+docker rm "java-build"
 
-#nginx dessert les fronts
+docker build -t "fit/nginx-build" -f $DIR/nginx/build/Dockerfile .
 
-#on build le Dockerfile qu'une seule fois
-docker build -t "fit/nginx-build" -f "nginx/build/Dockerfile" .
-
-#build le site vitrine
 docker run --name "nginx-build" -w="//opt/app/site_vitrine" "fit/nginx-build"
 docker cp "nginx-build":/opt/app/site_vitrine/dist dist/site_vitrine
 docker rm "nginx-build"
 
-#build l'intra gérant
 docker run --name "nginx-build" -w="//opt/app/intra_gerant" "fit/nginx-build" "yarn && yarn build"
 docker cp "nginx-build":/opt/app/intra_gerant/build dist/intra_gerant
 docker rm "nginx-build"
-
-#build intra-admin
-docker run --name "nginx-build" -w="//opt/app/intra_admin" "fit/nginx-build" "npm install && npm run-script build --prod"
-docker cp "nginx-build":/opt/app/intra_admin/build dist/intra_admin
-docker rm "nginx-build"
-
